cf579e7717a4029e797bf341bda130ed9da86b6c
Viva Chan <vivachan29@gmail.com>
2021-03-01 20:23:01 +0800

    chat function

---
+++ chat.py
@@ -0,0 +1,36 @@
+
+def read_file(filename):
+	lines = []
+	with open(filename , 'r' , encoding='utf-8-sig') as f:
+		for line in f:
+			lines.append(line.strip())
+	return lines
+
+def convert(lines):
+	new = []
+	person = None					# 沒有預設值
+	for line in lines:
+		if line == 'Allen':
+			person = 'Allen'
+			continue
+		elif line == 'Tom':
+			person = 'Tom'
+			continue
+		if person:					# 如果person有值 就跑下面的動作
+			new.append(person + ':' + line)
+	return new
+
+
+def write_file(filename , lines):
+	with open(filename , 'w' ) as f:
+		for line in lines:
+			f.write(line + '\n') 
+
+
+
+def main():
+	lines = read_file('input.txt')
+	lines = convert(lines)
+	write_file('ouput.txt', lines)
+
+main()
